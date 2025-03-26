00a469f79829e681ea9c6013d303f8c0a30fcb9e
Maximilian Krause <maxi.krause@me.com>
2025-03-26 20:43:51 +0100

    add rabbitmq broker

--- detection-service/main.py
+++ detection-service/main.py
@@ -1,12 +1,36 @@
 import os
 import time
+import socket
+import pika.exceptions
 from shared_logging.logging import logger
+import pika
 
 SCAN_DIR = "/mnt/scans"
+
 logger.info("Starting detection service...")
 if not os.path.exists(SCAN_DIR):
     logger.critical(f"{SCAN_DIR} does not exist!")
     exit(1)
+
+
+# Connect to RabbitMQ
+def connect_rabbitmq():
+    for i in range(10):
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq", heartbeat=30))
+            channel = connection.channel()
+            channel.queue_declare(queue="ocr_queue", durable=True)
+            return connection, channel
+        except (socket.gaierror, pika.exceptions.AMQPConnectionError):
+            time.sleep(2)
+    logger.critical("Couldn't connect to RabbitMQ.")
+    exit(2)
+
+
+# Verwende die Verbindung und den Kanal
+connection, channel = connect_rabbitmq()
+channel = connection.channel()
+channel.queue_declare(queue="ocr_queue", durable=True)
 
 logger.info(f"Scanning {SCAN_DIR} for new files...")
 
@@ -14,12 +38,19 @@ known_files = set(os.listdir(SCAN_DIR))
 
 while True:
     try:
+        connection.process_data_events(1)
         current_files = set(os.listdir(SCAN_DIR))
 
         new_files = current_files - known_files
         if new_files:
             for new_file in new_files:
                 logger.info(f"Found new file: {new_file}")
+                channel.basic_publish(
+                    exchange="",
+                    routing_key="ocr_queue",
+                    body=new_file,
+                    properties=pika.BasicProperties(delivery_mode=2)
+                )
 
         known_files = current_files
 
--- ocr-service/main.py
+++ ocr-service/main.py
@@ -1,6 +1,35 @@
-import time
+import pika
 from shared_logging.logging import logger
+import time
+import socket
 
 logger.info("Starting OCR service...")
-while True:
-    time.sleep(10)
+
+
+def callback(ch, method, properties, body):
+    logger.info(f"Received message: {body}")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+# Connect to RabbitMQ
+def connect_rabbitmq():
+    for i in range(10):
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq", heartbeat=30))
+            channel = connection.channel()
+            channel.queue_declare(queue="ocr_queue", durable=True)
+            return connection, channel
+        except (socket.gaierror, pika.exceptions.AMQPConnectionError):
+            time.sleep(2)
+    logger.critical("Couldn't connect to RabbitMQ.")
+    exit(2)
+
+
+# Verwende die Verbindung und den Kanal
+connection, channel = connect_rabbitmq()
+channel = connection.channel()
+
+channel.queue_declare(queue="ocr_queue", durable=True)
+channel.basic_consume(queue="ocr_queue", on_message_callback=callback)
+logger.info("OCR service ready!")
+channel.start_consuming()
