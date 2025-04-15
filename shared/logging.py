b061ec3afd794bde5488fd23fcc48f33e699d793
Maximilian Krause <maxi.krause@me.com>
2025-04-15 13:08:47 +0200

    add logfiles

--- shared/logging.py
+++ shared/logging.py
@@ -15,9 +15,20 @@ formatter = colorlog.ColoredFormatter(
     datefmt="%d.%m.%Y %H:%M:%S",
 )
 
+file_formatter = logging.Formatter(
+    "%(levelname)-8s %(asctime)s - %(filename)s:%(lineno)d - %(message)s",
+    datefmt="%d.%m.%Y %H:%M:%S",
+)
+
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
+file_handler = logging.FileHandler("logfile.log", encoding="utf-8")
+file_handler.setLevel(logging.WARNING)
+file_handler.setFormatter(file_formatter)
+
+# Logger einrichten
 logger = logging.getLogger("shared_logger")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(console_handler)
+logger.addHandler(file_handler)
